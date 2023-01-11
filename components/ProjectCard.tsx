0d2513fca373eb748924b7d86a6b2b42d278047d
erichvorster <erichvorster1@gmail.com>
2023-01-11 21:55:13 +0200

    added technologies grid

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -7,14 +7,15 @@ const ProjectCard = () => {
       initial={{ opacity: 0, scale: 0.9 }}
       whileInView={{ opacity: 1, scale: 1 }}
       transition={{ delay: 0.1 }}
+      className="rounded"
     >
       <motion.div
-        whileHover={{ scale: 1.08 }}
+        whileHover={{ scale: 1.1 }}
         whileTap={{ scale: 1 }}
         transition={{ type: "spring", stiffness: 500, damping: 17 }}
-        className="bg-neutral-800 p-10 m-2 group  "
+        className="bg-neutral-800 p-10 m-2 group rounded  "
       >
-        <div className="h-[3px] w-[120px] bg-neutral-500 mb-3 rounded-full group-hover:bg-indigo-500 group-hover:shadow-xxl group-hover:shadow-indigo-500/90  transition-all ease-in-out duration-300 "></div>
+        <div className="h-[3px] w-[120px]  bg-neutral-500 mb-3 rounded-full group-hover:bg-indigo-500 group-hover:shadow-xxl group-hover:shadow-indigo-500/90  transition-all ease-in-out duration-300 "></div>
         <div className="text-gray-300">
           <p className="mb-1 font-bold text-neutral-500 group-hover:text-[#EDEDED] transition-all ease-in-out duration-200 ">
             TYPESCRIPT
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,10 @@ const _document = () => {
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"
+        ></link>
       </Head>
       <body>
         <Main />
