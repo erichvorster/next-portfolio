6d323134a93c0871cfc2facf69638c81e5333d6a
erichvorster <erichvorster1@gmail.com>
2024-06-17 21:28:49 +0200

    update

--- components/ScrollLayout.tsx
+++ components/ScrollLayout.tsx
@@ -783,48 +783,27 @@ const ScrollLayout = () => {
                 : {}
             }
           >
-            <Link
-              href="https://www.atlassian.com/software/jira?&aceid=&adposition=&adgroup=140479881486&campaign=18442480203&creative=663390759269&device=c&keyword=jira&matchtype=e&network=g&placement=&ds_kids=p73335832032&ds_e=GOOGLE&ds_eid=700000001558501&ds_e1=GOOGLE&gclid=Cj0KCQjwldKmBhCCARIsAP-0rfyQArIqEJcOozyMu7UlX9iM_7cMAEAzS1t4JOg1wn7IARjSXtn0eTUaAtZBEALw_wcB&gclsrc=aw.ds"
-              target="_blank"
-            >
+            <Link href="https://www.figma.com/" target="_blank">
               <svg viewBox="0 0 128 128">
-                <defs>
-                  <linearGradient
-                    id="jira-original-a"
-                    gradientUnits="userSpaceOnUse"
-                    x1="22.034"
-                    y1="9.773"
-                    x2="17.118"
-                    y2="14.842"
-                    gradientTransform="scale(4)"
-                  >
-                    <stop offset=".176" stop-color="#0052cc"></stop>
-                    <stop offset="1" stop-color="#2684ff"></stop>
-                  </linearGradient>
-                  <linearGradient
-                    id="jira-original-b"
-                    gradientUnits="userSpaceOnUse"
-                    x1="16.641"
-                    y1="15.564"
-                    x2="10.957"
-                    y2="21.094"
-                    gradientTransform="scale(4)"
-                  >
-                    <stop offset=".176" stop-color="#0052cc"></stop>
-                    <stop offset="1" stop-color="#2684ff"></stop>
-                  </linearGradient>
-                </defs>
                 <path
-                  d="M108.023 16H61.805c0 11.52 9.324 20.848 20.847 20.848h8.5v8.226c0 11.52 9.328 20.848 20.848 20.848V19.977A3.98 3.98 0 00108.023 16zm0 0"
-                  fill="#2684ff"
+                  fill="#0acf83"
+                  d="M45.5 129c11.9 0 21.5-9.6 21.5-21.5V86H45.5C33.6 86 24 95.6 24 107.5S33.6 129 45.5 129zm0 0"
                 ></path>
                 <path
-                  d="M85.121 39.04H38.902c0 11.519 9.325 20.847 20.844 20.847h8.504v8.226c0 11.52 9.328 20.848 20.848 20.848V43.016a3.983 3.983 0 00-3.977-3.977zm0 0"
-                  fill="url(#jira-original-a)"
+                  fill="#a259ff"
+                  d="M24 64.5C24 52.6 33.6 43 45.5 43H67v43H45.5C33.6 86 24 76.4 24 64.5zm0 0"
                 ></path>
                 <path
-                  d="M62.219 62.078H16c0 11.524 9.324 20.848 20.848 20.848h8.5v8.23c0 11.52 9.328 20.844 20.847 20.844V66.059a3.984 3.984 0 00-3.976-3.98zm0 0"
-                  fill="url(#jira-original-b)"
+                  fill="#f24e1e"
+                  d="M24 21.5C24 9.6 33.6 0 45.5 0H67v43H45.5C33.6 43 24 33.4 24 21.5zm0 0"
+                ></path>
+                <path
+                  fill="#ff7262"
+                  d="M67 0h21.5C100.4 0 110 9.6 110 21.5S100.4 43 88.5 43H67zm0 0"
+                ></path>
+                <path
+                  fill="#1abcfe"
+                  d="M110 64.5c0 11.9-9.6 21.5-21.5 21.5S67 76.4 67 64.5 76.6 43 88.5 43 110 52.6 110 64.5zm0 0"
                 ></path>
               </svg>
             </Link>
